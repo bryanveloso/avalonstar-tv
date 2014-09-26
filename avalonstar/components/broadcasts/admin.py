93bf287da95a5c623d0a28357429126841950f4f
Bryan Veloso <bryan@revyver.com>
2014-09-26 16:42:33 -0700

    Basic admin stuffs.

--- avalonstar/components/broadcasts/admin.py
+++ avalonstar/components/broadcasts/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Broadcast, Series
+
+
+class BroadcastAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Broadcast, BroadcastAdmin)
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Series, SeriesAdmin)
--- avalonstar/components/games/admin.py
+++ avalonstar/components/games/admin.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game
+
+
+class GameAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Game, GameAdmin)
