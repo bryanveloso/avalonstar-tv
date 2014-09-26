5885c053e9bf20c7b91ebc2c8aebd1dfb9c78a46
Bryan Veloso <bryan@revyver.com>
2014-09-26 14:31:28 -0700

    Add the concept of series (like Whatever Wednesday).

--- avalonstar/components/broadcasts/models.py
+++ avalonstar/components/broadcasts/models.py
@@ -4,6 +4,13 @@ from django.db import models
 from components.games.models import Game
 
 
+class Series(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return '%s' % self.name
+
+
 class Broadcast(models.Model):
     airdate = models.DateField()
     status = models.CharField(max_length=200)
@@ -11,6 +18,8 @@ class Broadcast(models.Model):
 
     # ...
     games = models.ManyToManyField(Game, related_name='appears_on')
+    series = models.ForeignKey(Series, related_name='broadcasts')
 
+    # ...
     def __unicode__(self):
         return 'Episode %s' % self.number
