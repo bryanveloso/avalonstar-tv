eef65d4d136fdfdc376f0eb20cfc9a4907094c89
Bryan Veloso <bryan@revyver.com>
2014-09-26 20:48:27 -0700

    Static stuff?

--- avalonstar/urls.py
+++ avalonstar/urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'avalonstar.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+urlpatterns = patterns('',
+    # Administration Modules.
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
