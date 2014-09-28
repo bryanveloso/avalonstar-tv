efa01412b73bbde0678f7033b222004be752caa2
Bryan Veloso <bryan@revyver.com>
2014-09-28 11:35:38 -0700

    Another whoops.

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,5 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avalonstar.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
-from django.core.wsgi import get_wsgi_application
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
