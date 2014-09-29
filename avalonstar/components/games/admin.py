988d8fc74454e8bb54060219019bec64a5a7361d
Bryan Veloso <bryan@revyver.com>
2014-09-28 23:33:55 -0700

    GameAdmin + PlatformAdmin.

--- avalonstar/components/games/admin.py
+++ avalonstar/components/games/admin.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Game
+from .models import Game, Platform
 
 
 class GameAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'platform', 'gbid']
+
+    raw_id_fields = ['platform']
+    autocomplete_lookup_fields = { 'fk': ['series'] }
 admin.site.register(Game, GameAdmin)
+
+
+class PlatformAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Platform, PlatformAdmin)
