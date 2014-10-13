ed5dbd18273b937751a23596ca122909d504a727
Bryan Veloso <bryan@twitch.tv>
2014-10-12 20:19:54 -0700

    Moar URLs.

--- avalonstar/apps/live/urls.py
+++ avalonstar/apps/live/urls.py
@@ -2,10 +2,12 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-from .views import AwayView, BumperView
+from .views import AwayView, BumperView, GameView, PrologueView
 
 
 urlpatterns = patterns('',
     url(r'^/away/$', name='live-away', view=AwayView.as_view()),
     url(r'^/bumper/$', name='live-bumper', view=BumperView.as_view()),
+    url(r'^/game/$', name='live-game', view=GameView.as_view()),
+    url(r'^/prologue/$', name='live-prologue', view=PrologueView.as_view()),
 )
