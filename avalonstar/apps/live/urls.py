db2eb910a3dd50614dbd1687ac781704d8666f99
Bryan Veloso <bryan@twitch.tv>
2014-10-13 03:00:52 -0700

    Gaaaah slassshheess.

--- avalonstar/apps/live/urls.py
+++ avalonstar/apps/live/urls.py
@@ -6,8 +6,8 @@ from .views import AwayView, BumperView, GameView, PrologueView
 
 
 urlpatterns = patterns('',
-    url(r'^/away/$', name='live-away', view=AwayView.as_view()),
-    url(r'^/bumper/$', name='live-bumper', view=BumperView.as_view()),
-    url(r'^/game/$', name='live-game', view=GameView.as_view()),
-    url(r'^/prologue/$', name='live-prologue', view=PrologueView.as_view()),
+    url(r'^away/$', name='live-away', view=AwayView.as_view()),
+    url(r'^bumper/$', name='live-bumper', view=BumperView.as_view()),
+    url(r'^game/$', name='live-game', view=GameView.as_view()),
+    url(r'^prologue/$', name='live-prologue', view=PrologueView.as_view()),
 )
