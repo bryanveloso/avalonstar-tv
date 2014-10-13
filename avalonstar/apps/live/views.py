53ee7afe77e1c9ec452014245849ab05de642dd7
Bryan Veloso <bryan@twitch.tv>
2014-10-12 20:16:10 -0700

    Moar views.

--- avalonstar/apps/live/views.py
+++ avalonstar/apps/live/views.py
@@ -3,10 +3,16 @@ from django.views.generic import TemplateView
 
 
 class AwayView(TemplateView):
-    template_name = ''
-    pass
+    template_name = 'live/away.html'
 
 
 class BumperView(TemplateView):
-    template_name = ''
-    pass
+    template_name = 'live/bumper.html'
+
+
+class GameView(TemplateView):
+    template_name = 'live/game.html'
+
+
+class PrologueView(TemplateView):
+    template_name = 'live/prologue.view'
