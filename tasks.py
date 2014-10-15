f849134cffa733c5482142ff57ed087f4dd49dec
Bryan Veloso <bryan@revyver.com>
2014-10-14 17:46:10 -0700

    Add server and buff compile in tasks.py.

--- tasks.py
+++ tasks.py
@@ -15,11 +15,26 @@ def compile(verbose=False, **kwargs):
     hide = 'out' if not verbose else None
     STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'avalonstar', 'static')
 
-    # Compile the stylesheets.
+    # Autoprefix the stylesheets.
     run('autoprefixer -b "> 1%, last 3 versions, ff 17, opera 12.1" {input}'.format(
-        input=os.path.join(STATIC_ROOT, 'stylesheets', 'application.css')), hide=hide)
-    out('stylesheets/application.css auto-prefixed.')
+        input=os.path.join(STATIC_ROOT, 'stylesheets', 'site.css')), hide=hide)
+    out('stylesheets/site.css auto-prefixed.')
+    run('autoprefixer -b "> 1%, last 3 versions, ff 17, opera 12.1" {input}'.format(
+        input=os.path.join(STATIC_ROOT, 'stylesheets', 'live.css')), hide=hide)
+    out('stylesheets/live.css auto-prefixed.')
+
+    # Compile and minify the stylesheets.
     run('yuglify {input} --type css --combine {output}'.format(
-        input=os.path.join(STATIC_ROOT, 'stylesheets', 'application.css'),
-        output=os.path.join(STATIC_ROOT, 'stylesheets', 'production')), hide=hide)
-    out('stylesheets/production.min.css created and minified.')
+        input=os.path.join(STATIC_ROOT, 'stylesheets', 'live.css'),
+        output=os.path.join(STATIC_ROOT, 'stylesheets', 'live.min')), hide=hide)
+    out('stylesheets/live.min.css created and minified.')
+    run('yuglify {input} --type css --combine {output}'.format(
+        input=os.path.join(STATIC_ROOT, 'stylesheets', 'site.css'),
+        output=os.path.join(STATIC_ROOT, 'stylesheets', 'site.min')), hide=hide)
+    out('stylesheets/site.min.css created and minified.')
+
+
+@task
+def server(**kwargs):
+    # Use Foreman to start all the development processes.
+    run('foreman start -f Procfile.dev', pty=True)
