4e7888c22baf4db5f61b42c90f688a05c26052c0
Bryan Veloso <bryan@revyver.com>
2014-10-17 00:58:15 -0700

    All of the context processors.

--- avalonstar/settings/base.py
+++ avalonstar/settings/base.py
@@ -72,6 +72,16 @@ class Base(Configuration):
 
     # Template Configuration.
     # --------------------------------------------------------------------------
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        'django.contrib.auth.context_processors.auth',
+        'django.core.context_processors.debug',
+        'django.core.context_processors.i18n',
+        'django.core.context_processors.media',
+        'django.core.context_processors.static',
+        'django.core.context_processors.request',
+        'django.core.context_processors.tz',
+        'django.contrib.messages.context_processors.messages',
+    )
     TEMPLATE_DIRS = (normpath(join(DJANGO_ROOT, 'templates')),)
     TEMPLATE_LOADERS = (
         'django.template.loaders.filesystem.Loader',
