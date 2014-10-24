88d93f580ce2f587ac5fa1b41e7ab3f67a9c6be4
Bryan Veloso <bryan@revyver.com>
2014-10-24 15:11:35 -0700

    Add DiscussionView to the URLs.

--- avalonstar/apps/live/urls.py
+++ avalonstar/apps/live/urls.py
@@ -2,12 +2,14 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 
-from .views import AwayView, BumperView, GameView, PrologueView
+from .views import (AwayView, BumperView, DiscussionView, GameView,
+    PrologueView)
 
 
 urlpatterns = patterns('',
     url(r'^away/$', name='live-away', view=AwayView.as_view()),
     url(r'^bumper/$', name='live-bumper', view=BumperView.as_view()),
+    url(r'^discussion/$,' name='live-discussion', view=DiscussionView.as_view()),
     url(r'^game/$', name='live-game', view=GameView.as_view()),
     url(r'^prologue/$', name='live-prologue', view=PrologueView.as_view()),
 )
