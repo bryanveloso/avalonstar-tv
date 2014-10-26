f5115e0f1139e8879fbafc0cc0aad3845c4e138d
Bryan Veloso <bryan@revyver.com>
2014-10-25 19:00:02 -0700

    Add the rest of the API stuffs.

--- avalonstar/apps/api/urls.py
+++ avalonstar/apps/api/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, url, include
 
 from rest_framework import routers
 
-from .views import BroadcastViewSet
+from .views import BroadcastViewSet, RaidViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'broadcasts', BroadcastViewSet)
+router.register(r'raids', RaidViewSet)
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
--- avalonstar/apps/api/views.py
+++ avalonstar/apps/api/views.py
@@ -4,9 +4,15 @@ from rest_framework import viewsets
 from apps.broadcasts.models import Broadcast, Series
 from apps.games.models import Game
 
-from .serializers import BroadcastSerializer, GameSerializer, SeriesSerializer
+from .serializers import (BroadcastSerializer, GameSerializer, RaidSerializer,
+    SeriesSerializer)
 
 
 class BroadcastViewSet(viewsets.ModelViewSet):
     queryset = Broadcast.objects.all()
     serializer_class = BroadcastSerializer
+
+
+class RaidViewSet(viewsets.ModelViewSet):
+    queryset = Raid.objects.all()
+    serializer_class = RaidSerializer
