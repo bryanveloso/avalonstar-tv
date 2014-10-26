1acc54c5c84f0a1c5e3a780f31ea487436d4563b
Bryan Veloso <bryan@revyver.com>
2014-10-25 17:59:48 -0700

    Set the timezone as Los Angeles.

--- avalonstar/settings/base.py
+++ avalonstar/settings/base.py
@@ -66,7 +66,7 @@ class Base(Configuration):
     # Internationalization.
     # --------------------------------------------------------------------------
     LANGUAGE_CODE = 'en-us'
-    TIME_ZONE = 'UTC'
+    TIME_ZONE = 'America/Los_Angeles'
     USE_I18N = True
     USE_L10N = True
     USE_TZ = True
