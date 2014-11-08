7d83b7bede0bb984b9e83eb9f55229ca3190d703
Bryan Veloso <bryan@twitch.tv>
2014-11-07 17:37:23 -0800

    Redirect avalonstar.tv/live/ to the Twitch channel.

--- avalonstar/apps/live/urls.py
+++ avalonstar/apps/live/urls.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 from .views import AwayView, DiscussionView, GameView, PrologueView
 
 
 urlpatterns = patterns('',
+    # Because sooner or later, avalonstar.tv/ will be a welcome page.
+    url(r'^$', name='site-home', view=RedirectView.as_view(url='http://twitch.tv/avalonstar')),
+
+    # Overlays.
     url(r'^away/$', name='live-away', view=AwayView.as_view()),
     url(r'^discussion/$', name='live-discussion', view=DiscussionView.as_view()),
     url(r'^game/$', name='live-game', view=GameView.as_view()),
