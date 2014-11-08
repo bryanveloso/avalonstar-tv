8b04e4de91f60a2727f9c7a6f5a56d86279c667d
Bryan Veloso <bryan@twitch.tv>
2014-11-07 17:53:12 -0800

    Add a URL conf for the EpilogueView.

--- avalonstar/apps/live/urls.py
+++ avalonstar/apps/live/urls.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 
-from .views import AwayView, DiscussionView, GameView, PrologueView
+from .views import (AwayView, DiscussionView, EpilogueView, GameView,
+    PrologueView)
 
 
 urlpatterns = patterns('',
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     # Overlays.
     url(r'^away/$', name='live-away', view=AwayView.as_view()),
     url(r'^discussion/$', name='live-discussion', view=DiscussionView.as_view()),
+    url(r'^epilogue/$', name='live-epilogue', view=EpilogueView.as_view()),
     url(r'^game/$', name='live-game', view=GameView.as_view()),
     url(r'^prologue/$', name='live-prologue', view=PrologueView.as_view()),
 )
