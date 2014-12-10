6bfdf03ec427ff6cef12170be784eeba9c2af0d9
Bryan Veloso <bryan@revyver.com>
2014-12-10 02:43:17 -0800

    Add context for counts.

--- avalonstar/apps/broadcasts/views.py
+++ avalonstar/apps/broadcasts/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
 
-from .models import Broadcast
+from .models import Broadcast, Highlight, Raid
 
 
 class BroadcastDetailView(DetailView):
@@ -12,3 +12,9 @@ class BroadcastDetailView(DetailView):
 
 class BroadcastListView(ListView):
     model = Broadcast
+
+    def get_context_data(self, **kwargs):
+        context = super(BroadcastListView, self).get_context_data(**kwargs)
+        context['highlights'] = Highlight.objects.all()
+        context['raids'] = Raid.objects.all()
+        return context
