b0eaa365ed7d98319503f0027d355bd8d8eee857
Bryan Veloso <bryan@revyver.com>
2014-12-10 19:32:41 -0800

    Counts and intros and things.

--- avalonstar/apps/broadcasts/views.py
+++ avalonstar/apps/broadcasts/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
 
+from apps.games.models import Game
+
 from .models import Broadcast, Highlight, Raid
 
 
@@ -15,6 +17,8 @@ class BroadcastListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(BroadcastListView, self).get_context_data(**kwargs)
+        context['first'] = Broadcast.objects.earliest()
+        context['games'] = Game.objects.all()
         context['highlights'] = Highlight.objects.all()
         context['raids'] = Raid.objects.all()
         return context
