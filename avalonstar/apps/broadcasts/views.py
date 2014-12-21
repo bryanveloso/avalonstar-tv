3c3749c0dfa07c681796f5eff93112978d002df4
Bryan Veloso <bryan@revyver.com>
2014-12-21 15:09:08 -0800

    Statistics!

--- avalonstar/apps/broadcasts/views.py
+++ avalonstar/apps/broadcasts/views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import division
+
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
 
@@ -15,10 +17,27 @@ class BroadcastDetailView(DetailView):
 class BroadcastListView(ListView):
     model = Broadcast
 
+    def calculate_highlights_per_episode(self, **kwargs):
+        episodes = Broadcast.objects.count()
+        highlights = Highlight.objects.count()
+        return round((highlights / episodes), 2)
+
+    def calculate_raids_per_episode(self, **kwargs):
+        episodes = Broadcast.objects.count()
+        raids = Raid.objects.count()
+        return round((raids / episodes), 2)
+
     def get_context_data(self, **kwargs):
         context = super(BroadcastListView, self).get_context_data(**kwargs)
         context['first'] = Broadcast.objects.earliest()
         context['games'] = Game.objects.all()
+        context['games_completed'] = Game.objects.filter(is_completed=True)
+
+        # Highlights.
         context['highlights'] = Highlight.objects.all()
+        context['highlights_per_episode'] = self.calculate_highlights_per_episode()
+
+        # Raids.
         context['raids'] = Raid.objects.all()
+        context['raids_per_episode'] = self.calculate_raids_per_episode()
         return context
