727d37147a8f19c64d07c5fef3d0dddc2eec2b61
Bryan Veloso <bryan@revyver.com>
2014-12-27 12:27:02 -0800

    Cya dj-static, you were fun.

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avalonstar.settings')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
 from configurations.wsgi import get_wsgi_application
-from dj_static import Cling, MediaCling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
