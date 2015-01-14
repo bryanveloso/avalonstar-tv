54ef1ffd162bdeca9150acd9024ee60cc6019735
Bryan Veloso <bryan@revyver.com>
2015-01-13 20:53:37 -0800

    First pass at particles.

--- avalonstar/assets/javascripts/live.coffee
+++ avalonstar/assets/javascripts/live.coffee
@@ -7,3 +7,35 @@ $ ->
   # Specific adaptiveBackground execution.
   $(".game-image img").on "ab-color-found", (ev, payload) ->
     $(this).closest('.game').find('.game-metadata-marker').css('background-color', payload.color)
+
+  # Partciles for great justice!
+  particlesJS "particles-js",
+    particles:
+      color: "#090d12"
+      shape: "circle" # "circle", "edge" or "triangle"
+      opacity: 1
+      size: 3
+      size_random: true
+      nb: 200
+      line_linked:
+        enable_auto: true
+        distance: 75
+        color: "#090d12"
+        opacity: 1
+        width: 1
+        condensed_mode:
+          enable: true
+          rotateX: 600
+          rotateY: 600
+
+      anim:
+        enable: true
+        speed: 1
+
+    # We can't even click this thing inside of the browser
+    # plogin, so turn this off.
+    interactivity:
+      enable: false
+
+    # Retina Display Support.
+    retina_detect: true
--- avalonstar/static/javascripts/application/live.js
+++ avalonstar/static/javascripts/application/live.js
@@ -4,8 +4,38 @@
     $.adaptiveBackground.run({
       parent: ".game"
     });
-    return $(".game-image img").on("ab-color-found", function(ev, payload) {
+    $(".game-image img").on("ab-color-found", function(ev, payload) {
       return $(this).closest('.game').find('.game-metadata-marker').css('background-color', payload.color);
+    });
+    return particlesJS("particles-js", {
+      particles: {
+        color: "#090d12",
+        shape: "circle",
+        opacity: 1,
+        size: 3,
+        size_random: true,
+        nb: 200,
+        line_linked: {
+          enable_auto: true,
+          distance: 75,
+          color: "#090d12",
+          opacity: 1,
+          width: 1,
+          condensed_mode: {
+            enable: true,
+            rotateX: 600,
+            rotateY: 600
+          }
+        },
+        anim: {
+          enable: true,
+          speed: 1
+        }
+      },
+      interactivity: {
+        enable: false
+      },
+      retina_detect: true
     });
   });
 
