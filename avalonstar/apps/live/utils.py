3f9048a61aa7609bd829e7adc79bf46d907077cc
Bryan Veloso <bryan@revyver.com>
2015-02-22 11:18:42 -0800

    A working StatusView.

--- avalonstar/apps/live/utils.py
+++ avalonstar/apps/live/utils.py
@@ -5,26 +5,31 @@ import requests
 
 def fetch_stream():
     endpoint = 'https://api.twitch.tv/kraken/streams/avalonstar'
-    return requests.get(endpoint).json()
+    json = requests.get(endpoint).json()
+    return json.get('stream', {})
 
 
 def fetch_status():
     try:
         response = fetch_stream()
-        return response['stream']['channel']['status']
-    except TypeError:
+        return response.get('channel', {}).get('status', '')
+    except AttributeError, TypeError:
         return ''
 
 
 def is_episodic():
-    # Let's use the stream's title to determine if a stream is "casual"
-    # or not. The current way we determine this is as follows:
+    # Let's use the stream's title to determine if a stream is an episode
+    # or not. We use the stream's status to determine this as follows:
     #
     #   - "A☆###": A numbered episode.
     #   - (Anything else.): A casual episode.
     #
-    # Because Python is weird, it doesn't detect the white star, so we're not
+    # Because Python is weird, it doesn't detect the white star. We're not
     # going to bother looking for it.
     pattern = r'^A.\d{3}'
     status = fetch_status()
     return bool(re.match(pattern, status, re.UNICODE))
+
+
+def is_live():
+    return bool(fetch_stream())
