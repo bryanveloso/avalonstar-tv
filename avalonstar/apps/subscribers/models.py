e8c935e822278d2b9933eea00cce96c38af991c2
Bryan Veloso <bryan@revyver.com>
2015-02-22 18:12:51 -0800

    Add a basic Subscriber model.

---
+++ avalonstar/apps/subscribers/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Subscriber(models.Model):
+    twid = models.IntegerField()
+    name = models.CharField(max_length=200)
+    display_name = models.CharField(max_length=200)
+    subscribed = models.DateTimeField(help_text=u'When did the user subscribe?')
+
+    # Is this subscription active?
+    # TODO: Run a script that deactivates subs after XX days.
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['subscribed']
+
+    def __unicode__(self):
+        return u'%s' % self.name
