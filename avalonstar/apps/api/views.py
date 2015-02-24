a2e33263c27f763f39b9d5ada2c0892cfb9edccb
Bryan Veloso <bryan@revyver.com>
2015-02-23 17:45:20 -0800

    Retrieve tickets by username.

--- avalonstar/apps/api/views.py
+++ avalonstar/apps/api/views.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from apps.broadcasts.models import Broadcast, Raid, Series
 from apps.games.models import Game
@@ -19,6 +22,12 @@ class RaidViewSet(viewsets.ModelViewSet):
     serializer_class = RaidSerializer
 
 
-class TicketViewSet(viewsets.ModelViewSet):
+class TicketViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+    def retrieve(self, request, pk=None):
+        queryset = Ticket.objects.all()
+        ticket = get_object_or_404(queryset, name=pk)
+        serializer = TicketSerializer(ticket)
+        return Response(serializer.data)
