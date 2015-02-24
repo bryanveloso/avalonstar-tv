43e290506f7ea3e2913a695acb754e03804c818a
Bryan Veloso <bryan@revyver.com>
2015-02-24 12:10:36 -0800

    Add ordering to raids.

--- avalonstar/apps/broadcasts/models.py
+++ avalonstar/apps/broadcasts/models.py
@@ -104,6 +104,7 @@ class Raid(models.Model):
 
     class Meta:
         order_with_respect_to = u'broadcast'
+        ordering = ['timestamp']
 
     def __unicode__(self):
         return u'%s' % self.raider
