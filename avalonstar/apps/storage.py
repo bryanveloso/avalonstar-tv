4c5089ee6d425c2ba5cfc90e0b337c800d46efc0
Bryan Veloso <bryan@avalonstar.com>
2015-04-05 18:07:23 -0700

    This be defunct.

--- avalonstar/apps/storage.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib.staticfiles.storage import ManifestFilesMixin
-
-from storages.backends.s3boto import S3BotoStorage
-
-
-class ManifestStaticS3Storage(ManifestFilesMixin, S3BotoStorage):
-    pass
