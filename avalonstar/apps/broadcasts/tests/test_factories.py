9eac28a2bf8e07eaaff01b34765cd002d4db026d
Bryan Veloso <bryan@avalonstar.com>
2015-04-15 13:02:25 -0700

    Broadcast tests and things.

---
+++ avalonstar/apps/broadcasts/tests/test_factories.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from .factories import (BroadcastFactory, HighlightFactory,
+    HostFactory, RaidFactory, SeriesFactory)
+from ..models import Broadcast, Highlight, Host, Raid, Series
+
+pytestmark = pytest.mark.django_db
+
+
+def test_broadcast_factory():
+    factory = BroadcastFactory()
+    assert isinstance(factory, Broadcast)
+
+
+def test_highlight_factory():
+    factory = HighlightFactory()
+    assert isinstance(factory, Highlight)
+
+
+def test_host_factory():
+    factory = HostFactory()
+    assert isinstance(factory, Host)
+
+
+def test_raid_factory():
+    factory = RaidFactory()
+    assert isinstance(factory, Raid)
+
+
+def test_series_factory():
+    factory = SeriesFactory()
+    assert isinstance(factory, Series)
