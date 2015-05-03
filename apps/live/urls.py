a423b30cec75601728cd54f6e9198c55e8f6c3ff
Bryan Veloso <bryan@avalonstar.com>
2015-05-03 13:47:25 -0700

    Remove the overlay URLs.

--- apps/live/urls.py
+++ apps/live/urls.py
@@ -11,14 +11,6 @@ urlpatterns = patterns('',
     # Because sooner or later, avalonstar.tv/ will be a welcome page.
     url(r'^$', name='site-home', view=RedirectView.as_view(url='http://twitch.tv/avalonstar')),
 
-    # Overlays.
-    url(r'^away/$', name='live-away', view=AwayView.as_view()),
-    url(r'^discussion/$', name='live-discussion', view=DiscussionView.as_view()),
-    url(r'^epilogue/$', name='live-epilogue', view=EpilogueView.as_view()),
-    url(r'^game/$', name='live-game', view=GameView.as_view()),
-    url(r'^prologue/$', name='live-prologue', view=PrologueView.as_view()),
-    url(r'^notifier/$', name='live-notifier', view=NotifierView.as_view()),
-
     # Status (for bots).
     url(r'^status/$', name='live-status', view=StatusView.as_view()),
 )
