b4ebc8ae95db9f623c5afc7784110e570033adcc
Bryan Veloso <bryan@avalonstar.com>
2015-05-05 15:35:37 -0700

    Simple quote admin.

--- apps/quotes/admin.py
+++ apps/quotes/admin.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quote
+
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ['text', 'timestamp', 'creator', 'broadcast', 'game']
+admin.site.register(Quote, QuoteAdmin)
