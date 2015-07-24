09fdba4600ebcb8fdc894b6bb9b1abb774c55c64
Bryan Veloso <bryan@avalonstar.com>
2015-07-24 00:01:12 -0700

    Remove a RemovedInDjango19Warning.

--- apps/live/urls.py
+++ apps/live/urls.py
@@ -8,7 +8,7 @@ from .views import StatusView
 
 urlpatterns = patterns('',
     # Because sooner or later, avalonstar.tv/ will be a welcome page.
-    url(r'^$', name='site-home', view=RedirectView.as_view(url='http://twitch.tv/avalonstar')),
+    url(r'^$', name='site-home', view=RedirectView.as_view(url='http://twitch.tv/avalonstar', permanent=False)),
 
     # Status (for bots).
     url(r'^status/$', name='live-status', view=StatusView.as_view()),
